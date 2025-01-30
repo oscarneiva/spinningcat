74127a779e51900f00b612c0a082dcfea142e3bd
oscarneiva <oscarneivaeneto@gmail.com>
2025-01-30 18:57:00 -0300

    deploy

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import catinho from '/cat.gif'
+import './App.css'
+
+import { useRef } from "react";
+
+function App() {
+    const audioRef = useRef(null);
+
+    const playAudio = () => {
+        if (audioRef.current) {
+            audioRef.current.play();
+        }
+    };
+
+    return (
+        <div>
+            {/* Click the cat image to play the audio */}
+            <img src={catinho} className="logo" alt="Vite logo" onClick={playAudio} />
+
+            {/* Audio element (no autoplay) */}
+            <audio ref={audioRef} loop>
+                <source src="/catspin.mp3" type="audio/mp3"/>
+                Your browser does not support the audio element.
+            </audio>
+        </div>
+    );
+}
+
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function changeBackgroundColor() {
+    document.documentElement.style.setProperty('--bg-color', getRandomColor());
+}
+
+// Change background color every 3 seconds
+setInterval(changeBackgroundColor, 100);
+
+
+export default App
